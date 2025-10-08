8824c8ede306dc4f06f8a141291ab834fc70c558
BanLowi <lowi40k@gmail.com>
2025-10-08 14:31:38 +0200

    fourth task

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,7 +22,7 @@ const lastTeacher = teachers.pop();
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
